d2a141af542633e2597f0ca33e9f20f18bc9947f
Fisisy <18766736460@163.com>
2024-03-07 11:19:47 +0800

    提交了WXYAK内容

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Ke2sen AK ICPC!" <<std::endl;
+    std::cout << "wxy3265 AK ICPC EC Final!" << std::endl;
     return 0;
 }
